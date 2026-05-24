58dd1ac9e59c4340d58df1e430b7c49089e30a3f
Jasminbrusberg <J-brusberg@hotmail.com>
2026-05-24 20:55:48 +0200

    looper autoscrool sektionen ved hjælp af js

--- js/script.js
+++ js/script.js
@@ -30,3 +30,14 @@ for (const button of colorButtons) {
     button.classList.add("active");
   });
 }
+
+
+// laver loop sektion
+// const imgTrack=document.querySelector(".img-track"); finder listen med billeder
+const imageTrack = document.querySelector(".img-track");
+
+// const originalImages = imgTrack.innerHTML Gemmer alt indholdet inde i listen. Altså alle dine <li> med billeder.
+const originalImages = imageTrack.innerHTML;
+
+// imgTrack.innerHTML += originalImages; Kopierer billederne og sætter dem ind én gang mere. den hakker ikke
+imageTrack.innerHTML += originalImages;
