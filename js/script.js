0eba8f93934d74ad616768a46508d4389272b9cd
Jasminbrusberg <J-brusberg@hotmail.com>
2026-05-24 20:27:19 +0200

    små ændringer i js

--- js/script.js
+++ js/script.js
@@ -6,27 +6,27 @@ const productImage = document.querySelector("#product-image");
 // Finder alle farveknapperne
 const colorButtons = document.querySelectorAll(".color-option");
 
-// Looper alle farveknapper igennem
+// gennemgår alle farveknapper igennem
 for (const button of colorButtons) {
   button.addEventListener("click", function () {
     // Henter billedet fra data-image i HTML
     const newImage = button.dataset.image;
 
-    // Henter alt-teksten fra data-alt i HTML
+// Henter alt-teksten fra data-alt i HTML
     const newAlt = button.dataset.alt;
 
-    // Skifter produktbilledet
+// Skifter produktbilledet
     productImage.src = newImage;
 
-    // Skifter alt-teksten
+// Skifter alt-teksten
     productImage.alt = newAlt;
 
-    // Fjerner active fra alle farveknapper
+// Fjerner active fra alle farveknapper
     for (const colorButton of colorButtons) {
       colorButton.classList.remove("active");
     }
 
-    // Tilføjer active til den knap, brugeren klikkede på
+// Tilføjer active til den knap, brugeren klikkede på
     button.classList.add("active");
   });
 }
